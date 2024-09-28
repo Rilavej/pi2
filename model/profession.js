80e1056aa811f3d71429cf5cf07029750a91f658
Rilave <rilave.j1991@aluno.ifsc.edu.br>
2024-09-27 21:47:04 -0300

    Modelo Profession

---
+++ model/profession.js
@@ -0,0 +1,22 @@
+const sequelize = require('../config/dbconnection')
+const DataTypes = require('sequelize')
+
+// "Profession" pode subtituir "Professional" permitindo que uma pessoa tenha varias profissoes em unico cadastro
+const Profession = sequelize.define(
+    'Profession',
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        jobDescription: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        tableName: "profession"
+    }
+   
+)
+
+module.exports = Profession
